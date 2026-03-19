f1d2de07922355eaaee26d1857796db8209231f5
kristina-99 <kristina_atan@abv.bg>
2026-03-19 16:05:11 +0200

    * added a ReloadScene method in CrashDetector and FinishLine * added a delay before the level gets restarted

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,13 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float restartDelay = 1f;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
 
         if (collision.gameObject.layer == layerIndex)
         {
-            SceneManager.LoadScene(0);
+            Invoke("ReloadScene", restartDelay);
         }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
