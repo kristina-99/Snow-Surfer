7242e5b054fc48dcdb4a493dbc3d5a9b7ddf3126
kristina-99 <kristina_atan@abv.bg>
2026-03-20 19:21:00 +0200

    * added a Boost method in the PlayerController script to increase the player's speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,27 +4,53 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float baseSpeed = 11f;
+    [SerializeField] float boostSpeed = 16f;
+
+    SurfaceEffector2D surfaceEffector2D;
     InputAction moveAction;
     Rigidbody2D myRigidbody2D;
+    Vector2 moveVector;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidbody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveVector;
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+    void RotatePlayer()
+    {
         moveVector = moveAction.ReadValue<Vector2>();
+
         if (moveVector.x < 0)
         {
             myRigidbody2D.AddTorque(torqueAmount);
         }
-        else if(moveVector.x > 0)
+        else if (moveVector.x > 0)
         {
             myRigidbody2D.AddTorque(-torqueAmount);
+        }
+    }
+
+    void BoostPlayer()
+    {
+        if (moveVector.y > 0)
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
         }
     }
 }
